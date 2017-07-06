86fa9ed7b3f2e4f42636782c523533dbef0c0d8f
Bexultan Myrzatayev <a.e.iimitk@gmail.com>
2017-07-06 13:57:27 +0900

    set https

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,5 @@ class ApplicationController < ActionController::Base
   # protect_from_forgery with: :exception
   # skip_before_filter  :verify_authenticity_token
   # skip_before_action :verify_authenticity_token
+  protect_from_forgery with: :null_session
 end
