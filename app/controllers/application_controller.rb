a7f506a35340f76404ded4cd79e2c2e5a06d1b9a
Bexultan Myrzatayev <a.e.iimitk@gmail.com>
2017-07-25 19:17:47 +0600

    Experimenting with protect_from_forgery and set the log out

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,5 +2,6 @@ class ApplicationController < ActionController::Base
   # protect_from_forgery with: :exception
   # skip_before_filter  :verify_authenticity_token
   # skip_before_action :verify_authenticity_token
-  protect_from_forgery with: :null_session
+  # protect_from_forgery with: :null_session
+  protect_from_forgery prepend: true
 end
